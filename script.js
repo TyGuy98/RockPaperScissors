d8f0c70933da491cb06687b9d8708bc5ad864d59
TyGuy98 <LittleCharon@protonmail.com>
2024-09-11 17:04:50 +0100

    Update checkHumanChoice function
    Instead of asking user to type the full string, we can have it check for just the first letter only.

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 
 // Function to get a random choice by the computer
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    return randomNumber;
+
+    if (randomNumber === 1) {
+        return "rock";
+    }
+    else if (randomNumber === 2){
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
 }
 
-function getHumanChoice (string) {
-    string = string.toLowerCase();
-    return string;
-    
-}
 
-// Function to check user input is valid
+// Function to check user input and convert it to "rock", "paper", or "scissors"
 function checkHumanChoice (string) {
-    if (string === "rock") {
-        return string;
+    string = string.toLowerCase();
+
+    if (string === "rock" || string == "r") {
+        return string = "rock";
     }
 
-    else if (string === "paper") {
-        return string;
+    else if (string === "paper" || string === "p") {
+        return string = "paper";
     }
 
-    else if (string === "scissors") {
-        return string;
+    else if (string === "scissors" || string === "s") {
+        return string = "scissors";
     }
 
     else {
@@ -34,28 +40,79 @@ function checkHumanChoice (string) {
     }
 }
 
-function compareScore (humanChoice, computerChoice) {
+// Function to compare user input to the computer's value 
+function compareChoice (humanChoice, computerChoice) {
 
-    if (humanChoice === "rock" && computerChoice === 1 || humanChoice === "paper" && computerChoice === 2 || humanChoice === "scissors" && computerChoice === 3 ){
-        alert(`Computer chose ${humanChoice}, Play on!`);
+    if (humanChoice === computerChoice){
+        alert(`Both have chosen ${humanChoice}, Play on!`);
     }
 
-    else if (humanChoice == "rock" )
+    else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock" ){
+
+        alert(`Computer has chosen ${computerChoice}, 1 point to the computer!`);
+        computerScore++;
+    }
+
+    else {
+        alert(`Computer has chosen ${computerChoice}, you won 1 point!`);
+        humanScore++;
+    }
 
 }
 
+// Function to play one round
+function playRound () {
 
-function playRound (humanChoice, computerChoice) {
+    let humanChoice;
 
     // Loop to keep prompting user if the input is not valid 
     do {
-        humanChoice = getHumanChoice((prompt("Please pick between Rock, Paper or Scissors!")));
-    } while(!checkHumanChoice(humanChoice));
+        let input = prompt("Please pick between Rock (R), Paper (P) or Scissors (S)!");
+        humanChoice = checkHumanChoice(input);
+    } while(!checkHumanChoice(humanChoice)); // Loop until a valid input is entered
    
-    computerChoice = getComputerChoice();
-    
-
-
+    let computerChoice = getComputerChoice();
+    compareChoice(humanChoice, computerChoice);
 }
 
-playRound();
+// Function to ask if the user wants to play again
+function playAgain (string) {
+    let userInput;
+    humanScore = 0;
+    computerScore = 0;
+
+    do {
+        userInput = prompt("Would you like to play again? (Yes/No)").toLowerCase(); 
+    } while (userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n");
+
+    userInput === "yes" || userInput === "y" ? playGame() :  alert("Thank you for playing!");
+}
+   
+// FUnction to play full game
+function playGame() {
+
+    // Play for 5 rounds
+    for( let i = 1; i <= 5; i++) {
+        playRound();
+    }
+    if (humanScore > computerScore){
+        alert(`You beat the computer's score ${computerScore} with a score of ${humanScore}. Well done!` )
+    }
+
+    else if (humanScore < computerScore) {
+        alert(`You lost to the computer's score ${computerScore} with a score of ${humanScore}. Better luck next time!` )
+    }
+
+    else {
+        alert(`The game comes to a tie with both a score of ${computerScore}!`)
+    }
+
+    // After 5 rounds, ask user if they want to play again
+    playAgain();
+    
+    
+}
+
+// Inital startr
+playGame();
+
